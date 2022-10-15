9cafac197ff41d08202ce575256029be9b3f2194
Anatoly <glenglen74@gmail.com>
2022-10-15 13:54:57 +0500

    1.1

---
+++ MultiProjects/Service/src/main/java/Phone.java
@@ -0,0 +1,10 @@
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Phone {
+    @NonNull private final String prefix;
+    @NonNull private final String number;
+    private String bonusCode;
+}
